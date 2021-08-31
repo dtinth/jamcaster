57b8517dd44e6e2584294cb3f6097425480af976
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-31 19:41:21 +0000

    Start X server

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
-: ${ICECAST_MOUNT_POINT:?"Missing required environment variable ICECAST_MOUNT_POINT"}
 : ${JAMULUS_SERVER:?"Missing required environment variable JAMULUS_SERVER"}
 
-exec supervisord
+if [ -n "$VIDEO_STREAM_TARGET" ]
+then
+  # Video mode
+  exec xvfb-run --server-args="-screen 0 1280x720x24" supervisord
+else
+  # Audio mode
+  : ${ICECAST_MOUNT_POINT:?"Missing required environment variable ICECAST_MOUNT_POINT"}
+  exec supervisord
+fi
