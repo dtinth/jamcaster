10741a5ed9e14bbf76003723221378b49920b941
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-01 11:24:20 +0000

    Display error messages when Xvfb cannot start

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -10,7 +10,7 @@ elif [ -n "$VIDEO_STREAM_TARGET" ]
 then
   # Video mode
   cp ./src/supervisor-video.conf /etc/supervisor/conf.d/video.conf
-  exec xvfb-run -f /tmp/xauth --server-args="-screen 0 1280x720x24" supervisord
+  exec xvfb-run -e /dev/stderr -f /tmp/xauth --server-args="-screen 0 1280x720x24" supervisord
 else
   # Audio mode
   : ${ICECAST_MOUNT_POINT:?"Missing required environment variable ICECAST_MOUNT_POINT"}
