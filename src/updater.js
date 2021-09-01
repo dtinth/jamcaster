72eb009cd188f45ac87ec52870bea76ed0128503
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-01 13:53:03 +0000

    Return immediately if no icecast

--- src/updater.js
+++ src/updater.js
@@ -1,57 +1,61 @@
-require("make-promises-safe");
+require('make-promises-safe')
 
-const delay = require("delay");
-const axios = require("axios");
-const jayson = require("jayson/promise");
-const jamulusClient = new jayson.client.tcp({ host: "127.0.0.1", port: 22100 });
-let currentName;
+const delay = require('delay')
+const axios = require('axios')
+const jayson = require('jayson/promise')
+const jamulusClient = new jayson.client.tcp({ host: '127.0.0.1', port: 22100 })
+let currentName
 
 async function main() {
-  const icecastMountPoint = process.env.ICECAST_MOUNT_POINT;
-  const mountPointPathname = new URL(icecastMountPoint).pathname;
-  const nameTemplate = process.env.JAMULUS_CLIENT_NAME || "%s";
-  const statusUrl = getStatusUrl(icecastMountPoint);
+  const icecastMountPoint = process.env.ICECAST_MOUNT_POINT
+  if (!icecastMountPoint) {
+    return
+  }
+
+  const mountPointPathname = new URL(icecastMountPoint).pathname
+  const nameTemplate = process.env.JAMULUS_CLIENT_NAME || '%s'
+  const statusUrl = getStatusUrl(icecastMountPoint)
 
   for (;;) {
-    await delay(1000);
+    await delay(1000)
     try {
-      const response = await axios.get(statusUrl);
+      const response = await axios.get(statusUrl)
       let {
         icestats: { source },
-      } = response.data;
+      } = response.data
       if (!Array.isArray(source)) {
-        source = [source].filter((x) => x);
+        source = [source].filter((x) => x)
       }
 
       const matchingSource = source.find((x) => {
-        return new URL(x.listenurl).pathname === mountPointPathname;
-      });
+        return new URL(x.listenurl).pathname === mountPointPathname
+      })
       if (!matchingSource) {
-        console.error("[No matching source found]");
-        continue;
+        console.error('[No matching source found]')
+        continue
       }
 
-      const name = nameTemplate.replace("%s", matchingSource.listeners);
+      const name = nameTemplate.replace('%s', matchingSource.listeners)
       if (name !== currentName) {
-        currentName = name;
-        console.log(`Updating client name to ${name}`);
+        currentName = name
+        console.log(`Updating client name to ${name}`)
         jamulusClient
-          .request("jamulusclient/setName", { name })
-          .then(console.log, console.error);
+          .request('jamulusclient/setName', { name })
+          .then(console.log, console.error)
       }
     } catch (error) {
-      console.error(error);
+      console.error(error)
     } finally {
-      await delay(4000);
+      await delay(4000)
     }
   }
 }
 
 function getStatusUrl(icecastMountPoint) {
-  const url = new URL(icecastMountPoint);
-  url.pathname = "/status-json.xsl";
-  url.protocol = "http:";
-  return url.toString();
+  const url = new URL(icecastMountPoint)
+  url.pathname = '/status-json.xsl'
+  url.protocol = 'http:'
+  return url.toString()
 }
 
-main();
+main()
